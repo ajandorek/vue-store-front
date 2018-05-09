62539630dc06744ad094a60260fdd4bb5cf6f118
Alec Jandorek <ajandorek@gmail.com>
2018-05-09 15:26:30 -0500

    Created POST route for new items with dummy data

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('./models/Items');
 
 const app = express();
-
+const Item = mongoose.model('items');
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect('mongodb://localhost:27017/storefront');
 
 app.get('/', (req, res) => {
   res.send('It works!');
+});
+
+app.post('/api/add_product', (req, res) => {
+  Item.create({
+    name: 'Test',
+    description: 'Test',
+    price: 100,
+    productId: 1,
+    department: 'Sports'
+  }).then(obj => res.json(obj));
 });
 
 app.listen(PORT, () => {
