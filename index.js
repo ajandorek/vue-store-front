e52fb49b4f7f866fc1fc2e810a5518cbdd19ed2a
Alec Jandorek <ajandorek@gmail.com>
2018-05-21 16:14:11 -0500

    Added body parser

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 const Item = mongoose.model('items');
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 mongoose.connect('mongodb://localhost:27017/storefront');
 
 app.get('/', (req, res) => {
